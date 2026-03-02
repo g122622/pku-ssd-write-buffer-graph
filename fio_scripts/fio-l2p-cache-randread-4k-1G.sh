bc04d67ef9aaa1e54930a61aa523d85736b49ba8
g122622 <3300925806@qq.com>
2026-03-02 22:50:26 +0800

    feat: 修改脚本保存路径

--- fio_scripts/fio-l2p-cache-randread-4k-1G.sh
+++ fio_scripts/fio-l2p-cache-randread-4k-1G.sh
@@ -4,7 +4,7 @@ OFFSET="1G"
 SIZE="1G"
 
 # 创建结果目录（如果不存在）
-RESULTS_DIR=~/exp_results
+RESULTS_DIR=/mnt/d/MiscProjects/pku-ssd-write-buffer-graph/results/fio-l2p-cache-randread-4k-1G
 mkdir -p $RESULTS_DIR
 
 # 获取当前时间戳，用于文件名
